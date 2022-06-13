ddc09ecd0c5e65fe03ae7fed717d20e5afb7f6b9
JustusWamswa <93210719+JustusWamswa@users.noreply.github.com>
2022-06-13 23:21:08 +0400

    New AboutUs Page

--- pages/AboutUs.js
+++ pages/AboutUs.js
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components"
+import Head from 'next/head';
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return <Wrapper className="w-5/6">
+    <Head>
+      {/* <!--syne font--> */}
+      <link href="https://fonts.googleapis.com/css2?family=Syne:wght@600&display=swap" rel="stylesheet" />
+    </Head>
+    <div>
+      <h1 className="text-6xl font-bold pb-10">About Us</h1>
+    </div>
+    <p className="text-3xl leading-loose">Welcome to Global Green Visa, a gateway to environmental tourism. Working closely with national and International partners,
+      GGV assists governments to deliver the environmental and climate benefits promised in the Paris Accords.
+    </p>
+  </Wrapper>;
 };
 
+const Wrapper = styled.div`
+  font-family: 'Syne', sans-serif;
+`;
 export default AboutUs;
