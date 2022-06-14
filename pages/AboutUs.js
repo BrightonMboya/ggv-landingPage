bc935902879d6052bf14a1c3888f228cfd9d5c76
JustusWamswa <93210719+JustusWamswa@users.noreply.github.com>
2022-06-14 11:06:56 +0400

    Added bottom styling

--- pages/AboutUs.js
+++ pages/AboutUs.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 import Head from 'next/head';
 
 const AboutUs = () => {
-  return <Wrapper className="w-5/6">
+  return <Wrapper className="w-5/6 flex flex-col justify-center items-center">
     <Head>
       {/* <!--syne font--> */}
       <link href="https://fonts.googleapis.com/css2?family=Syne:wght@600&display=swap" rel="stylesheet" />
@@ -14,6 +14,13 @@ const AboutUs = () => {
     <p className="text-xl leading-loose sm:text-3xl sm:leading-loose">Welcome to Global Green Visa, a gateway to environmental tourism. Working closely with national and International partners,
       GGV assists governments to deliver the environmental and climate benefits promised in the Paris Accords.
     </p>
+    <div className="flex flex-col justify-center items-center absolute bottom-2">
+      <div className="flex justify-center items-center rounded-3xl border-white border-2 border-solid w-7 h-16">
+        <hr className="rotate-90 border-l w-8" />
+      </div>
+      <p className="text-lg">Join Us</p>
+    </div>
+
   </Wrapper>;
 };
 
